2f95552f7ec2de6a86e43204ed275ba7dab96e97
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2024-08-04 17:59:58 +0600

    Add Swift DocC Documentation

--- ToDoToday/Models/SampleTasks.swift
+++ ToDoToday/Models/SampleTasks.swift
@@ -7,9 +7,11 @@
 
 import Foundation
 
+/// A type that contains sample instances of ``TaskToDo`` used for debugging.
 struct SampleTasks {
     private init() { }
     
+    /// Sample instances of ``TaskToDo``
     static let content: [TaskToDo] = [
         TaskToDo(title: "Breakfast", time: getTime(hour: 07, minutes: 30), taskDetails: "Early morning breakfast, yum! Egg is always a good choice. Skipping the breakfast is not a good idea."),
         TaskToDo(title: "Laundry", time: getTime(hour: 18, minutes: 30), taskDetails: "Only on Saturday and Tuesday. Not a fan but must do it."),
@@ -19,6 +21,15 @@ struct SampleTasks {
         TaskToDo(title: "Previous Task", time: getTime(hour: 20, minutes: 30, day: 20), taskDetails: "This task should be deleted.")
     ]
     
+    /// Get a specific date by hour, minutes, and day.
+    /// 
+    /// The `day` parameter is optional. If no argument is passed to the `day` parameter, it defaults to today's day.
+    /// 
+    /// - Parameters:
+    ///   - hour: The hour for the date.
+    ///   - minutes: The minutes for the date.
+    ///   - day: The day for the date.
+    /// - Returns: An instance of a `Date` representing the specified hour, minutes, and day.
     private static func getTime(hour: Int, minutes: Int, day: Int? = nil) -> Date {
         var defaultComponents = Calendar.current.dateComponents([.day, .month, .year], from: .now)
         defaultComponents.hour = hour
--- ToDoToday/Support/Color-Theme.swift
+++ ToDoToday/Support/Color-Theme.swift
@@ -9,10 +9,12 @@ import Foundation
 import SwiftUI
 
 extension Color {
+    /// Background color to display when a task is tapped and the color scheme is light.
     static var listLightBackground: Color {
         Color(red: 0.35, green: 0.6, blue: 0.9)
     }
     
+    /// Background color to display when a task is tapped and the color scheme is dark.
     static var listDarkBackground: Color  {
         Color(red: 0.1, green: 0.2, blue: 0.45)
     }
